7153c21d47b1435fd385f40f07993978e6a31978
luqilinok <luqilinok@live.cn>
2018-09-08 00:11:22 +0800

    baiduren

---
+++ 06.图/PFS.h
@@ -0,0 +1,47 @@
+﻿template<typename Tv, typename Te>
+template<typename PU>
+inline void Graph<Tv, Te>::pfs(int s, PU prioUpdater)
+{
+	reset();
+	int v = s;
+	do
+	{
+		if (UNDISCOVERED = status(v))
+		{
+			PFS(v, prioUpdater);
+		}
+	} while (s != (v = (++v%n)));
+}
+
+
+template<typename Tv, typename Te>
+template<typename PU>
+inline void Graph<Tv, Te>::PFS(int s, PU prioUpdater)
+{
+	priority(s) = 0;
+	status(s) = VISITED;
+	parent(s) = -1;
+	while (1)
+	{
+		for (int w = 0; w > -1; w = nextNbr(s, w))
+		{
+			prioUpdater(this, s, w);
+		}
+		for (int shortest = INT_MAX, w = 0; w < n; w++)
+		{
+			if (status(w) == UNDISCOVERED)
+			{
+				if (shortest > priority(w))
+				{
+					shortest = priority(w);
+					s = w;
+				}
+			}
+		}
+		if (VISITED == status(s))
+		{
+			status(s) = VISITED;
+			status(parent(s), s) = TREE;
+		}
+	}
+}
