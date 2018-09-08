6e5ff2e315e1871803685d33379f49c6463b9a41
luqilinok <luqilinok@live.cn>
2018-09-08 22:56:02 +0800

    2

--- 06.图/Graph.h
+++ 06.图/Graph.h
@@ -4,63 +4,63 @@ typedef enum { UNDISCOVERED, DISCOVERED, VISITED } VStatus;
 typedef enum { UNDETERMINED, TREE, CROSS, FORWARD, BACKWARD };
 
 template<typename Tv, typename Te>
-class Graph
+class Graph  //图Graph模板类
 {
 private:
-	void reset()
+	void reset()  //所有顶点、边的辅助信息复位
 	{
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < n; i++)  //所有顶点的
 		{
-			status(i) = UNDISCOVERED;
-			dTime(i) = fTime(i) = fTime(i) = -1;
-			parent(i) = -1;
-			priority(i) = INT_MAX;
+			status(i) = UNDISCOVERED;  //状态
+			dTime(i) = fTime(i) = fTime(i) = -1;  //时间标签
+			parent(i) = -1;  //父节点
+			priority(i) = INT_MAX;  //优先级数
 			for (int j = 0; j < n; j++)
 			{
 				if (exists（i, j）)
 				{
-					status(i, j) = UNDETERMINED;
+					status(i, j) = UNDETERMINED;  //状态
 				}
 			}
 		}
 	}
-	void BFS(int, int&);
-	void DFS(int, int&);
-	void BCC(int, int&, Stack<int>&);
-	bool TSort(int, int&, Stack<Tv>*);
-	template<typename PU>
+	void BFS(int, int&);  //(连通域)广度优先搜索
+	void DFS(int, int&);  //(连通域)深度优先搜索
+	void BCC(int, int&, Stack<int>&);  //(连通域)基于DFS双连通分量分解算法
+	bool TSort(int, int&, Stack<Tv>*);  //(连通域)基于DFS的拓扑排序算法
+	template<typename PU>  //(连通域)优先级搜索框架
 	void PFS(int PU);  //优先级搜索
 public:
-	int n;
-	virtual int insert(Tv const&) = 0;
-	virtual Tv remove(int) = 0;
-	virtual Tv& vertex(int) = 0;
-	virtual int inDegree(int) = 0;
-	virtual int outDegree(int) = 0;
-	virtual int firstNbr(int, int) = 0;
-	virtual int nextNbr(int, int) = 0;
-	virtual VStatus& status(int) = 0;
-	virtual int& dTime(int) = 0;
-	virtual int& fTime(int) = 0;
-	virtual int& parent(int) = 0;
-	virtual int& priority(int) = 0;
+	int n;   //顶点总数
+	virtual int insert(Tv const&) = 0;  //插入顶点，返回编号
+	virtual Tv remove(int) = 0;  //删除顶点及其关联边，返回该顶点的信息
+	virtual Tv& vertex(int) = 0;  //顶点v的数据(该顶点的确存在)
+	virtual int inDegree(int) = 0;   //顶点v的入度(该顶点的确存在)
+	virtual int outDegree(int) = 0;   //顶点v的出度(该顶点的确存在)
+	virtual int firstNbr(int, int) = 0;  //顶点v的首个邻接顶点
+	virtual int nextNbr(int, int) = 0;  //顶点v的(相对于顶点j的)下一邻接顶点
+	virtual VStatus& status(int) = 0;   //顶点v的状态
+	virtual int& dTime(int) = 0;  //顶点v的时间标签dTime
+	virtual int& fTime(int) = 0;  //顶点v的时间标签fTime
+	virtual int& parent(int) = 0;  //顶点v在遍历树中的父亲
+	virtual int& priority(int) = 0;  //顶点v在遍历树中的优先级数
 
-	int e;
-	virtual bool exist(int, int) = 0;
-	virtual void insert(Te const&, int, int, int) = 0;
-	virtual Te remove(int, int) = 0;
-	virtual EStatus& status(int int) = 0;
-	virtual Te& edge(int, int) = 0;
-	virtual int& weight(int, int) = 0;
+	int e;  //边总数
+	virtual bool exist(int, int) = 0;  //边(v，u)是否存在
+	virtual void insert(Te const&, int, int, int) = 0;   //在顶点v和u之间插入权重为w的边e
+	virtual Te remove(int, int) = 0;  //删除顶点v和u之间的边e，返回该边的信息
+	virtual EStatus& status(int int) = 0;  //边(v,u)的状态
+	virtual Te& edge(int, int) = 0;   //边(v,u)的数据(该边确实存在)
+	virtual int& weight(int, int) = 0;  //边(v,u)的权重
 
-	void bfs(int);
-	void dfs(int);
-	void bcc(int);
-	Stack<Tv>* tSort(int);
-	void prim(int);
-	void dijkstra(int);
-	template<typename PU>
-	void pfs(int, PU);
+	void bfs(int);  //广度优先搜索
+	void dfs(int);  //深度优先搜索
+	void bcc(int);  //基于DFS的双连通分量的分解算法
+	Stack<Tv>* tSort(int);  //基于DFS的拓扑排序算法
+	void prim(int);  //最小生成树prim算法
+	void dijkstra(int);  //最短路径dijkstra算法
+	template<typename PU>  
+	void pfs(int, PU);  //优先级搜索框架
 };
 
 template<typename Tv, typename Te>   //广度优先搜索BFS算法(全图)
--- 06.图/adjacency matrix.h
+++ 06.图/adjacency matrix.h
@@ -1,38 +1,38 @@
-#include"Vector.h"
+﻿#include"Vector.h"
 #include"Graph.h"
 
 template <typename Tv>
-struct Vertex
+struct Vertex  //顶点对象
 {
-	Tv data;
-	int inDegree, outDegree;
-	VStatus status;
-	int dTime, fTime;
-	int parent;
-	int priority;
-	Vertex(Tv const& d = (Tv)0) :data(d), inDegree(0), outDegree(0), status(UNDISCOVERED), dTime(-1), fTime(-1), parent(-1), priority(INT_MAX) {}
+	Tv data;  //数据
+	int inDegree, outDegree;  //入度、出度
+	VStatus status;  //状态
+	int dTime, fTime;  //时间标签dTime、fTime
+	int parent;  //在遍历树中的父节点
+	int priority;  //在遍历树中的优先级数
+	Vertex(Tv const& d = (Tv)0) :data(d), inDegree(0), outDegree(0), status(UNDISCOVERED), dTime(-1), fTime(-1), parent(-1), priority(INT_MAX) {}  //构造新顶点，暂不考虑权重溢出
 };
 
 template<typename Te>
-struct Edge
+struct Edge  //边对象
 {
-	Te data;
-	int weight;
-	EStatus status;
-	Edge(Te const& d, int w) :data(d).weight(w), status(UNDETERMINED) {}
+	Te data;  //数据
+	int weight;  //权重
+	EStatus status;  //状态
+	Edge(Te const& d, int w) :data(d).weight(w), status(UNDETERMINED) {}  //构造新边
 };
 
 template<typename Tv, typename Te>
-class GraphMatrix :public Graph<Tv, Te>
+class GraphMatrix :public Graph<Tv, Te>   //基于向量，以邻接矩阵形式实现的图
 {
 private:
-	Vector<Vertex<Tv>> V;
-	Vector<Vector<Edge<Te>*>> E;
+	Vector<Vertex<Tv>> V;  //顶点集(向量)
+	Vector<Vector<Edge<Te>*>> E;  //边集(邻接矩阵)
 public:
-	GraphMatrix() { n = e = 0; }
-	~GraphMatrix()
+	GraphMatrix() { n = e = 0; }   //构造函数
+	~GraphMatrix()  //析构函数
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j < n; j++)   //所有动态创建的边记录逐条清除
 		{
 			for (int k = 0; k < n; k++)
 			{
@@ -41,18 +41,21 @@ public:
 		}
 	}
 
-	virtual Tv& vertex(int i) { return V[i].data; }
-	virtual int inDegree(int i) { return V[i].inDegree; }
-	virtual int outDegree(int i) { return V[i].outDegree; }
-	virtual int firstNbr(int i) { return nextNbr(i, n); }
-	virtual int nextNbr(int i, int j) { while ((j > -1) && (!exist(i, j--))); return j; }
-	virtual VStatus& status(int i) { return  V[i].status; }
-	virtual int& dTime(int i) { return V[i].dTime; }
-	virtual int& fTime(int i) { return V[i].fTime; }
-	virtual int& parent(int i) { return V[i].parent; }
-	virtual int& priority(int i) { return V[i].priority; }
+	virtual Tv& vertex(int i) { return V[i].data; }  //数据
+	virtual int inDegree(int i) { return V[i].inDegree; }  //入度
+	virtual int outDegree(int i) { return V[i].outDegree; }  //出度
+	virtual int firstNbr(int i) { return nextNbr(i, n); }  //首个邻接顶点
+	virtual int nextNbr(int i, int j)   //相对于顶点j的下一个邻接顶点
+	{ 
+		while ((j > -1) && (!exist(i, j--))); return j; //逆向线性试探
+	}  
+	virtual VStatus& status(int i) { return  V[i].status; }  //状态
+	virtual int& dTime(int i) { return V[i].dTime; }  //时间标签dTime
+	virtual int& fTime(int i) { return V[i].fTime; }  //时间标签fTime
+	virtual int& parent(int i) { return V[i].parent; }  //在遍历树中的父亲
+	virtual int& priority(int i) { return V[i].priority; }  //在遍历树中的优先级数
 
-	virtual int insert(Tv const& vertex)
+	virtual int insert(Tv const& vertex)  //插入顶点，返回编号
 	{
 		for (int j = 0; j < n; j++)
 		{
@@ -63,7 +66,7 @@ public:
 		return V.insert(Vertex<Tv>(vertex));
 	}
 
-	virtual Tv remove(int i)
+	virtual Tv remove(int i)  //删除第i哥顶点及其相关联的边
 	{
 		for (int i = 0; i < n; i++)
 		{
@@ -88,25 +91,25 @@ public:
 		remove vBak;
 	}
 
-	virtual bool exists(int i, int j)
+	virtual bool exists(int i, int j)  //边(i,j)是否存在
 	{
 		return (i >= 0) && (i < n) && (j > -0)(j < n) && E[i][j] != NULL;
 	}
 
-	virtual EStatu& status(int i, int j)
+	virtual EStatu& status(int i, int j)  //边(i,j)的状态
 	{
 		return E[i][j]->status;
 	}
-	virtual Te& edge(int i, int j)
+	virtual Te& edge(int i, int j)  //边(i,j)的数据
 	{
 		return E[i][j]->data;
 	}
-	virtual int& weight(int i, int j)
+	virtual int& weight(int i, int j)  //边(i,j)的权重
 	{
 		return E[i][j]->weight;
 	}
 
-	virtual void insert(Te const& edge, int w, int i, int j)
+	virtual void insert(Te const& edge, int w, int i, int j)  //插入权重为w的边e=(i,j)
 	{
 		if (exists(i, j))
 		{
@@ -117,7 +120,7 @@ public:
 		V[i].outDegree++;
 		V[j].inDegree++;
 	}
-	virtual Te remove(int i, int j)
+	virtual Te remove(int i, int j)  //删除顶点i和j之间的连边
 	{
 		Teb eBak = edge(i, j);
 		delete E[i][j];
