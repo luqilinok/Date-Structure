50a36b4dd98dbd1ede16afcaec9c147f20e19f0b
luqilinok <luqilinok@live.cn>
2018-09-08 21:08:30 +0800

    add index

--- 03.列表/List.h
+++ 03.列表/List.h
@@ -1,4 +1,4 @@
-typedef int Rank;  //秩
+﻿typedef int Rank;  //秩
 #define ListNodePosi(T) ListNode<T>*
 
 template<typename T> struct ListNode  //列表节点模板类
@@ -25,50 +25,53 @@ private:
 
 protected:
 	void init();  //列表创建时的初始化
-	int clear();
-	void copyNodes(ListNodePosi(T), int n);
-	void merge(ListNodePosi(T)&, int, List<T>&, ListNodePosi(T), int);
-	void mergeSort(ListNodePosi(T)&, int);
-	void selectionSort(ListNodePosi(T), int);
-	void insertionSort(ListNodePosi(T), int);
+	int clear();  //清除所有节点
+	void copyNodes(ListNodePosi(T), int n);  //复制列表中自位置p起的n项
+	void merge(ListNodePosi(T)&, int, List<T>&, ListNodePosi(T), int);  //有序列表区间归并
+	void mergeSort(ListNodePosi(T)&, int);  //对从p开始连续的n个节点归并排序
+	void selectionSort(ListNodePosi(T), int);  //对从p开始连续的n个节点选择排序
+	void insertionSort(ListNodePosi(T), int);  //对从p开始连续的n个节点插入排序
 
 public:
-	List() { init(); }
-	List(List<T> const& L);
-	List(List<T> const& l, Rank r, int n);
-	List(ListNodePosi(T) p, int n);
+	List() { init(); }  //默认构造函数
+	List(List<T> const& L);  //整体复制列表L(拷贝构造函数)
+	List(List<T> const& l, Rank r, int n);  //复制列表L中自第r项起的n项
+	List(ListNodePosi(T) p, int n);  //复制列表中自位置p起的n项
 
-	~List();
+	~List();  //析构函数，释放所有节点
 
-	Rank size() const { return _size; }
-	bool empty() const { return _size <= 0; }
-	T& operator[](Rank r) const;
-	ListNodePosi(T) first() const { return header->succ; }
-	ListNodePosi(T) last() const { return trailer->pred; }
-	bool valid(ListNodePosi(T) p) { return p && (trailer != p) && (header != p); }
-	int disorder() const;
-	ListNodePosi(T) find(T const& e)const { return find(e, _size, trailer); }
-	ListNodePosi(T) find(T const& e, int n, ListNodePosi(T) p) const;
-	ListNodePosi(T) search(T const& e) const { return search(e, _size, trailer); }
-	ListNodePosi(T) search(T const& e, int n, ListNodePosi(T) p) const;
-	ListNodePosi(T) selectMax(ListNodePosi(T) p, int n);
-	ListNodePosi(T) selectMax() { return selectMax(header->succ, _size); }
+	Rank size() const { return _size; }  //规模
+	bool empty() const { return _size <= 0; }  //判空
+	T& operator[](Rank r) const;   //重载，支持循秩访问
+	ListNodePosi(T) first() const { return header->succ; }  //首节点位置
+	ListNodePosi(T) last() const { return trailer->pred; }  //末节点位置
+	bool valid(ListNodePosi(T) p) //判断位置p是否对外合法
+	{ 
+		return p && (trailer != p) && (header != p); //将头、尾节点等同于NULL
+	}  
+	int disorder() const;  //判断列表是否已排序
+	ListNodePosi(T) find(T const& e)const { return find(e, _size, trailer); }  //无序列表查找
+	ListNodePosi(T) find(T const& e, int n, ListNodePosi(T) p) const;  //无序区间查找
+	ListNodePosi(T) search(T const& e) const { return search(e, _size, trailer); }  //有序列表查找
+	ListNodePosi(T) search(T const& e, int n, ListNodePosi(T) p) const;  //有序区间查找
+	ListNodePosi(T) selectMax(ListNodePosi(T) p, int n);  //在p及其前n-1哥后继中选出最大者
+	ListNodePosi(T) selectMax() { return selectMax(header->succ, _size); }  //整体最大者
 
-	ListNodePosi(T) insertAsFirst(T const& e);
-	ListNodePosi(T) insertAsLast(T const& e);
-	ListNodePosi(T) insertBefore(ListNodePosi(T) p, T const& e);
-	ListNodePosi(T) insertAfter(ListNodePosi(T) p, T const& e);
-	T remove(ListNodePosi(T) p);
-	void merge(List<T>& L) { merge(first(), size, L, L.first(), L._size); }
-	void sort(ListNodePosi(T) p, int n);
-	void sort() { sort(first(), _size); }
-	int deduplicate();
-	int uniquify();
-	void reverse();
+	ListNodePosi(T) insertAsFirst(T const& e);  //将e当作首节点插入
+	ListNodePosi(T) insertAsLast(T const& e);  //将e当作末节点插入
+	ListNodePosi(T) insertBefore(ListNodePosi(T) p, T const& e);  //将e当作p的前驱插入
+	ListNodePosi(T) insertAfter(ListNodePosi(T) p, T const& e);  //将e当作p的后继插入
+	T remove(ListNodePosi(T) p);  //删除合法位置p出的节点，返回被删除节点
+	void merge(List<T>& L) { merge(first(), size, L, L.first(), L._size); }  //全列表归并
+	void sort(ListNodePosi(T) p, int n);  //列表区间排序
+	void sort() { sort(first(), _size); }  //列表整体排序
+	int deduplicate();  //无序去重
+	int uniquify();  //有序去重
+	void reverse();  //前后倒置
 
-	void traverse(void(*T)(T&));
-	template<typename VST>
-	void traverse(VST&);
+	void traverse(void(*T)(T&));  //遍历，依次实施visit操作(函数指针，只读或局部性修改)
+	template<typename VST>  //操作器
+	void traverse(VST&);  //遍历，依次实施visit操作(函数对象，可全局性修改)
 };
 
 template<typename T>
