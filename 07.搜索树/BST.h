1cc3497b514db216948d56de1585014fef785712
luqilinok <luqilinok@live.cn>
2018-09-08 14:31:02 +0800

    baidu

---
+++ 07.搜索树/BST.h
@@ -0,0 +1,33 @@
+﻿#include"BinTree.h"
+
+template<typename T>
+class BST :public BinTree<T>
+{
+protected:
+	BinNodePosi(T) _hot;
+	BinNodePosi(T) connect34(
+		BinNodePosi(T), BinNodePosi(T), BinNodePosi(T),
+		BinNodePosi(T), BinNodePosi(T), BinNodePosi(T), BinNodePosi(T));
+	BinNodePosi(T) rotateAt(BinNodePosi(T) x);
+public:
+	virtual BinNodePosi(T)&  search(const T& e);
+	virtual BinNodePosi(T) insert(const T& e);
+	virtual bool remove(const T& e);
+};
+
+template<typename T>
+static BinNodePosi(T) & searchIn(BinNodePosi(T)& v, const T& e, BinNodePosi(T)& hot)
+{
+	if (!v||(e==v->data))
+	{
+		return v;
+	}
+	hot = v;
+	return searchIn(((e < v->data) ? v->lChild : v->rChild), e, hot);
+}
+
+template<typename T>
+BinNodePosi(T)& BST<T>::search(const T& e)
+{
+	return searchIn(_root, e, _hot = NULL);
+}
---
+++ 07.搜索树/Entry.h
@@ -0,0 +1,12 @@
+﻿template<typename K,typename V>
+struct Entry  //词条模板类
+{
+	K key; //关键码
+	V value;  //数值
+	Entry(K k = K(), V v = V()) :key(k), value(v) {};
+	Entry(Entry<K, V> const& e) :key(e.key), value(e.value) {};
+	bool operator<(Entry<K, V> const&e) { return key < e.key; }
+	bool operator>(Entry<K, V> const&e) { return key > e.key; }
+	bool operator==(Entry<K, V> const&e) { return key = e.key; }
+	bool operator!=(Entry<K, V> const&e) { return key != e.key; }
+};
