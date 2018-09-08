798c8bebd6ec35667be1f828981a265a048e3cfe
luqilinok <luqilinok@live.cn>
2018-09-08 15:19:41 +0800

    baidu

--- 07.搜索树/BST.h
+++ 07.搜索树/BST.h
@@ -31,3 +31,63 @@ BinNodePosi(T)& BST<T>::search(const T& e)
 {
 	return searchIn(_root, e, _hot = NULL);
 }
+
+template<typename T>
+inline BinNodePosi(T) BST<T>::insert(const T & e)
+{
+	BinNodePosi(T) & x = search(e);
+	if (x)
+	{
+		return x;
+	}
+	x = new BinNode<T>(e, _hot);
+	_size++;
+	updateHeightAbove(x);
+	return x;
+}
+
+#include"BinTree.h"
+
+template<typename T>
+static BinNodePosi(T) removeAt(BinNodePosi(T)& x, BinNodePosi(T)& hot)
+{
+	BinNodePosi(T) w = x;
+	BinNodePosi(T) succ = NULL;
+	if (!HasLChild(*x))
+	{
+		succ = x->rChild;
+	}
+	else if (!HasRChild(*x))
+	{
+		succ = x->rChild;
+	}
+	else
+	{
+		w = w->succ();
+		swap(x->data, w->data);
+		BinNodePosi(T) u = w->parent;
+		((u == x) ? u->rChild : u->rChild) = succ = w->rChild;
+	}
+	hot = w->parent;
+	if (succ)
+	{
+		succ->parent = hot;
+	}
+	release(w->data);
+	release(w);
+	release succ;
+}
+
+template<typename T>
+inline bool BST<T>::remove(const T & e)
+{
+	BinNodePosi(T) & x = search(e);
+	if (!x)
+	{
+		return false;
+	}
+	removeAt(x, _hot);
+	_size--;
+	updateHeightAbove(_hot);
+	return false;
+}
