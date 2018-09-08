ba10b1de163900c271724fbd789fb8dc46d855b9
luqilinok <luqilinok@live.cn>
2018-09-08 21:13:07 +0800

    栈修改

--- 04.栈和队列/Queue.h
+++ 04.栈和队列/Queue.h
@@ -1,9 +1,9 @@
-#include"List.h"
+﻿#include"List.h"  //以List为基类
 
-template<typename T>class Queue :public List<T>
+template<typename T>class Queue :public List<T>  //队列模板类
 {
 public:
-	void enqueue(T const& e) { insertAsLast(e); }
-	T dequeue{ return remove(first()); }
-	T& front() { return first()->data; }
+	void enqueue(T const& e) { insertAsLast(e); }  //入队:尾部插入
+	T dequeue{ return remove(first()); }  //出队:首部删除
+	T& front() { return first()->data; }  //队首
 };
--- 04.栈和队列/Stack.h
+++ 04.栈和队列/Stack.h
@@ -1,9 +1,9 @@
-#include"Vector.h"
+﻿#include"Vector.h"  //以向量为基类，派生出栈模板类
 
-template<typename T> class Stack :public Vector<T>
+template<typename T> class Stack :public Vector<T>   //将向量的首/末端作为栈底/顶
 {
 public:
-	void push(T const& e) { insert(size(), e); }
-	T pop() { return remove(size() - 1); }
-	T& top() { return (*this)[size() - 1]; }
+	void push(T const& e) { insert(size(), e); }  //入栈:等效于将新元素作为向量的末元素插入
+	T pop() { return remove(size() - 1); }  //删除:等效于删除向量的末元素
+	T& top() { return (*this)[size() - 1]; }  //取顶:直接返回向量的末元素
 };
