5c2a25c05208a261a70e5e0da163a162d62d7554
luqilinok <luqilinok@live.cn>
2018-09-08 14:07:34 +0800

    baiduer

---
+++ 06.图/Dijkstra.h
@@ -0,0 +1,17 @@
+﻿#include"Graph.h"
+
+template<typename Tv,typename Te>   //针对Dijkstra算法的顶点优先级更新器
+struct DijkstraPU
+{
+	virtual void operator()(Graph<Tv, Te>* g, int uk, int v)
+	{
+		if (g->status(v)==DISCOVERED)
+		{
+			if (g->priority(v)>g->priority(uk)+g->weight(uk,v))
+			{
+				g->priority(v) = g->priority(uk) + g->weight(uk, v);
+				g->parent(v) = uk;
+			}
+		}
+	}
+};
