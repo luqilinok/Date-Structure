12c358a439c882920b3f0df6d10227eac53fed2a
luqilinok <luqilinok@live.cn>
2018-09-08 21:25:58 +0800

    二叉树注释更新完成

--- 05.二叉树/BinNode.h
+++ 05.二叉树/BinNode.h
@@ -33,31 +33,31 @@
 #define stature(p)((p)?(p)->height:-1) //节点亮度（与“空树高度为-1”的约定相统一）
 typedef enum { RB_RED, RB_BLACK } RBColor; //节点颜色
 
-template<typename T>struct BinNode
+template<typename T>struct BinNode  //二叉树节点模板类
 {
-	T data;
-	BinNodePosi(T) parent;
-	BinNodePosi(T) lChild;
-	BinNodePosi(T) rChild;
+	T data;  //数值
+	BinNodePosi(T) parent;  //父节点
+	BinNodePosi(T) lChild;  //左孩子
+	BinNodePosi(T) rChild;  //右孩子
 
-	int height;
+	int height;  //高度
 	int npl;  //Null Path Length(左式堆，也可直接使用height代替)
-	RBColor color;
-
-	BinNode() :parent(NULL), lChild(NULL), rChild(NULL), height(0), npl(1), color(RB_RED) {}
+	RBColor color;  //颜色(红黑树)
+	//构造函数
+	BinNode() :parent(NULL), lChild(NULL), rChild(NULL), height(0), npl(1), color(RB_RED) {}  
 	BinNode(T e, BinNodePosi(T) p = NULL, BinNodePosi(T) lc = NULL, BinNodePosi(T) rc = NULL, int h = 0, int l = 1, RBColor c = RB_RED) :data(e), parent(p), lChild(lc), rChild(rc), height(h), npl(l), color(c) {}
 
-	int size();
-	BinNodePosi(T) insertAsLC(T const&);
-	BinNodePosi(T) insertAsRC(T const&);
-	BinNodePosi(T) succ();  //定位节点
-	template<typename VST> void travLevel(VST&);
-	template<typename VST> void travPre(VST&);
-	template<typename VST> void travIn(VST&);
-	template<typename VST> void travPost(VST&);
+	int size();  //统计当前节点后代总数
+	BinNodePosi(T) insertAsLC(T const&);  //作为当前节点的左孩子插入新节点
+	BinNodePosi(T) insertAsRC(T const&);  //作为当前节点的右孩子插入新节点
+	BinNodePosi(T) succ();  //取当前节点的直接后继
+	template<typename VST> void travLevel(VST&);  //子树层次遍历
+	template<typename VST> void travPre(VST&);   //子树先序遍历
+	template<typename VST> void travIn(VST&);  //子树中序遍历
+	template<typename VST> void travPost(VST&);  //子树后续遍历
 
-	bool operator<(BinNode const& bn) { return data < bn.data; }
-	bool operator==(BinNode const& bn) { return data == bn.data; }
+	bool operator<(BinNode const& bn) { return data < bn.data; }  //小于
+	bool operator==(BinNode const& bn) { return data == bn.data; }  //等于
 };
 
 template<typename T>
--- 05.二叉树/BinTree.h
+++ 05.二叉树/BinTree.h
@@ -1,35 +1,39 @@
-#include"BinNode.h"
+﻿#include"BinNode.h"  
 #include"Stack.h"
 
-template<typename T>
+template<typename T>  //二叉树模板类
 class BinTree
 {
 protected:
-	int _size;
-	BinNodePosi(T) _root;
-	virtual int updateHeight(BinNodePosi(T) x);
-	void updateHeightAbove(BinNodePosi(T) x);
+	int _size;  //规模
+	BinNodePosi(T) _root;  //根节点
+	virtual int updateHeight(BinNodePosi(T) x);  //更新节点x的高度
+	void updateHeightAbove(BinNodePosi(T) x);  //更新节点x及其祖先的高度
 public:
-	BinTree() :_size(0), _root(NULL) {}
-	~BinTree() { if (_size > 0)remove(_root); }
-	int size() const { return _size(); }
-	bool empty() const { return !_root; }
-	BinNodePosi(T) root() const { return _root; }
-	BinNodePosi(T) insertAsRoot(T const& e);
-	BinNodePosi(T) insertAsLC(BinNodePosi(T) x, T const& e);
-	BinNodePosi(T) insertAsRC(BinNodePosi(T) x, T const& e);
-	BinNodePosi(T) attachAsLC(BinNodePosi(T) x, BinTree<T>* &T);
-	BinNodePosi(T) attachAsRC(BinNodePosi(T) x, BinTree<T>* &T);
-	int remove(BinNodePosi(T) x);
-	BinTree<T>* secede(BinNodePosi(T) x);
+	BinTree() :_size(0), _root(NULL) {}  //构造函数
+	~BinTree() { if (_size > 0)remove(_root); }  //析构函数
+	int size() const { return _size(); }  //规模
+	bool empty() const { return !_root; }  //判空
+	BinNodePosi(T) root() const { return _root; }  //树根
+	BinNodePosi(T) insertAsRoot(T const& e);  //插入根节点
+	BinNodePosi(T) insertAsLC(BinNodePosi(T) x, T const& e);  //e作为x的左孩子(原来没有)插入
+	BinNodePosi(T) insertAsRC(BinNodePosi(T) x, T const& e);  //e作为x的右孩子(原来没有)插入
+	BinNodePosi(T) attachAsLC(BinNodePosi(T) x, BinTree<T>* &T);  //T作为x左子树接入
+	BinNodePosi(T) attachAsRC(BinNodePosi(T) x, BinTree<T>* &T);  //T作为x右子树接入
+	int remove(BinNodePosi(T) x);  //删除以位置x处节点为根的子树，返回该子树原先的规模
+	BinTree<T>* secede(BinNodePosi(T) x);  //将子树x从当前树中删除，并将其转换为一棵独立的的子树
+	
+	template<typename VST>  
+	void travLevel(VST& visit) { if (_root)_root->travLevel(visit); }  //层次遍历
+	
 	template<typename VST>
-	void travLevel(VST& visit) { if (_root)_root->travLevel(visit); }
+	void travPre(VST& visit) { if (_root)_root->travPre(visit); }  //先序遍历
+	
 	template<typename VST>
-	void travPre(VST& visit) { if (_root)_root->travPre(visit); }
+	void travIn(VST& visit) { if (_root)_root->travIn(visit); }  //中序遍历
+	
 	template<typename VST>
-	void travIn(VST& visit) { if (_root)_root->travIn(visit); }
-	template<typename VST>
-	void travPost(VST& visit) { if (_root)_root->travPost(visit); }
+	void travPost(VST& visit) { if (_root)_root->travPost(visit); }  //后序遍历
 
 	bool operator<(BinTree<T> const& t) { return _root && t._root&&lt(_root, t._root); }
 	bool operator==(BinTree<T> const& t) { return _root && t._root && (_root == t._root); }
