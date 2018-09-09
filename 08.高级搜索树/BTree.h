695e59198bca93b144ba0bb94477e7da8e4d9ff0
luqilinok <luqilinok@live.cn>
2018-09-09 21:26:18 +0800

    update

--- 08.高级搜索树/BTree.h
+++ 08.高级搜索树/BTree.h
@@ -27,3 +27,101 @@ public:
 	bool insert(const T& e);  //插入
 	bool remove(const T& e);  //删除
 };
+
+template<typename T>
+void BTree<T>::solveOverflow(BTNodePosi(T) v)
+{
+	if (_order >= v->child.size())
+	{
+		return;
+	}
+	Rank s = _order / 2;
+	BTNodePosi(T) u = new BTNode<T>();
+	for (Rank j = 0; j < _order - s - 1; j++)
+	{
+		u->child.insert(j, v->child.remove(s + 1));
+		u->key.insert(j, v->key.remove(s + 1));
+	}
+	u->child[_order - s - 1] = v->child.remove(s + 1);
+	if (u->child[0])
+	{
+		for (Rank j = 0; j < _order - s; j++)
+		{
+			u->child[j]->parent = u;
+		}
+		BTNodePosi(T) p = v->parent;
+		if (!p)
+		{
+			_root = p = new BTNode<T>();
+			p->child[0] = v;
+			v->parent = p;
+		}
+		Rank r = 1 + p->key.search(v->key[0]);
+		p->key.insert(r, v->key.remove(s));
+		p->child.insert(r + 1, u);
+		u->parent = p;
+		solveOverflow(p);
+	}
+}
+
+template<typename T>
+inline BTNodePosi(T) BTree<T>::search(const T & e)
+{
+	BTNodePosi(T) v = _root;
+	_hot = NULL;
+	while (v)
+	{
+		Rank r = v->key.search(e);
+		if ((r >= 0) && (e == v->key(r)))
+		{
+			return v;
+		}
+		_hot = v;
+		v = v->child[r + 1];
+	}
+	return NULL;
+}
+
+template<typename T>
+inline bool BTree<T>::insert(const T & e)
+{
+	BTNodePosi(T) v = search(e);
+	if (v)
+	{
+		return false;
+	}
+	Rank r = _hot->key.search(e);
+	_hot->key.insert(r + 1, e);
+	_hot->child.insert(r + 2, NULL);
+	_size++;
+	solveOverflow(_hot);
+	return true;
+}
+
+
+template<typename T>
+bool BTree<T>::remove(const T& e)
+{
+	BTNodePosi(T) v = search(e);
+	if (!v)
+	{
+		return false;
+	}
+	Rank r = v->key.search(e);
+	if (v->child[0])
+	{
+		BTNodePosi(T) u = v->child[r + 1];
+		while (u->child[0])
+		{
+			u = u->child[0];
+		}
+		v->key[r] = u->key[0];
+		v = u;
+		r = 0;
+	}
+	v->key.remove(r);
+	v->child.remove(r + 1);
+	_size--;
+	solveUnderflow(v);
+	return true;
+}
