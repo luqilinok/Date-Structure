65aac1c542f83541e62d1528d862eb24763c5655
luqilinok <luqilinok@live.cn>
2018-09-09 17:06:25 +0800

    2

---
+++ 08.高级搜索树/BTNode.h
@@ -0,0 +1,27 @@
+﻿#include"Vector.h"
+#define BTNodePosi(T) BTNode<T>*
+
+template<typename T>
+struct BTNode   //B-树节点模板类
+{
+	BTNodePosi(T) parent;   //父节点
+	Vector<T> key;  //关联向量码
+	Vector<BTNodePosi(T)> child;  //孩子向量
+
+	BTNode() { parent = NULL; child.insert(0, NULL); }
+	BTNode(T e, BTNodePosi(T) lc = NULL, BTNodePosi(T) rc = NULL)
+	{
+		parent = NULL;  //作为根节点，而且初始时只有一个关键码，以及两个孩子
+		key.insert(0, e);
+		child.insert(0, lc);
+		child.insert(1, rc);
+		if (lc)
+		{
+			lc->parent = this;
+		}
+		if (rc)
+		{
+			rc->parent = this;
+		}
+	}
+};
---
+++ 08.高级搜索树/BTree.h
@@ -0,0 +1,29 @@
+﻿#include"BTNode.h"
+
+template<typename T>
+class BTree  //B-树模板类
+{
+protected:
+	int _size;  //存放的关键码总数
+	int _order;  //B-树的阶次，至少为3
+	BTNodePosi(T) _root;  //根节点  
+	BTNodePosi(T) _hot;  //search最后访问的非空的节点的位置
+	void solveOverflow(BTNodePosi(T));  //因插入而上溢之后的分裂处理
+	void solveUnderflow(BTNodePosi(T));  //因删除而下溢之后的合并处理
+public:
+	BTree(int order = 3) :_order(order), _size(0) { _root = new BTNode<T>(); }  //构造函数:默认为最低的3阶
+	~BTree()  //析构函数
+	{
+		if (_root)
+		{
+			release(_root);
+		}
+	}
+	int const order() { return _order; }  //阶次
+	int const size() { return _size; }  //规模
+	BTNodePosi(T)& root() { return _root; }  //树根
+	bool empty()const { return !_root; }  //判空
+	BTNodePosi(T) search(const T& e);  //查找
+	bool insert(const T& e);  //插入
+	bool remove(const T& e);  //删除
+};
