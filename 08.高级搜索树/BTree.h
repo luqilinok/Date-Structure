8f5206c57e0a9beab8b1b8cfa43dff17b1f52c19
luqilinok <luqilinok@live.cn>
2018-09-10 09:40:57 +0800

    baiduplayer

--- 08.高级搜索树/BTree.h
+++ 08.高级搜索树/BTree.h
@@ -6,7 +6,7 @@ class BTree  //B-树模板类
 protected:
 	int _size;  //存放的关键码总数
 	int _order;  //B-树的阶次，至少为3
-	BTNodePosi(T) _root;  //根节点  
+	BTNodePosi(T) _root;  //根节点
 	BTNodePosi(T) _hot;  //search最后访问的非空的节点的位置
 	void solveOverflow(BTNodePosi(T));  //因插入而上溢之后的分裂处理
 	void solveUnderflow(BTNodePosi(T));  //因删除而下溢之后的合并处理
@@ -98,7 +98,6 @@ inline bool BTree<T>::insert(const T & e)
 	return true;
 }
 
-
 template<typename T>
 bool BTree<T>::remove(const T& e)
 {
@@ -124,4 +123,103 @@ bool BTree<T>::remove(const T& e)
 	_size--;
 	solveUnderflow(v);
 	return true;
+}
+
+template<typename T>
+void BTree<T>::solveUnderflow(BTNodePosi(T) v)
+{
+	if ((_order + 1) / 2 <= v->child.size())
+	{
+		return;
+	}
+	BTNodePosi(T) p = v->parent;
+	if (!p)
+	{
+		if (!v->key.size() && v->child[0])
+		{
+			_root = v->child[0];
+			_root->parent = NULL;
+			v->child[0] = NULL;
+			release(v);
+		}
+		return;
+	}
+	Rank r = 0;
+	while (p->child[r] != v)
+	{
+		r++;
+	}
+	if (r > 0)
+	{
+		BTNodePosi(T) ls = p->child[r - 1];
+		if ((_order + 1 / 2) < ls->child.size())
+		{
+			v->key.insert(0, p->key[r - 1]);
+			p->key[r - 1] = ls->key.remove(ls->key.size() - 1);
+			v->child.insert(-, ls->child.remove(ls->child.size() - 1));
+			if (v->child[0])
+			{
+				v->child[0]->parent = v;
+				return;
+			}
+		}
+		if (p->child.size() - 1 > r)
+		{
+			BTNodePosi(T) rs = p->child[r + 1];
+			if ((_order + 1) / 2 < rs->child.size())
+			{
+				v->key.insert(v->key.size(), p->key[r]);
+				p->key[r] = rs->key.remove(0);
+				v->child.insert(v->child.size(), rs->child.remove(0));
+
+				if (v->child[v->child.size() - 1])
+				{
+					v->child[v->child.size() - 1]->parent = v;
+				}
+				return;
+			}
+		}
+	}
+
+	if (r > 0)
+	{
+		BTNodePosi(T) ls = p->child[r - 1];
+		ls->key.insert(ls->child.size(), v->child.remove(0));
+		if (ls->child[ls->child.size() - 1])
+		{
+			ls->child[ls->child.size() - 1]->parent = ls;
+		}
+		while (!v->key.empty())
+		{
+			ls->key.insert(ls->key.size(), v->key.remove(0));
+			ls->child.insert(ls->child.size(), v->child.remove(0));
+			if (ls->child[ls->child.size() - 1])
+			{
+				ls->child[ls->child.size() - 1]->parent = ls;
+			}
+			release(v);
+		}
+		else
+		{
+			BTNodePosi(T) rs = p->child[r + 1];
+			rs->key.insert(0, p->key.remove(r));
+			p->child.remove(r);
+			rs->child.insert(0, v->child.remove(v->child.size() - 1));
+			if (rs->child[0])
+			{
+				rs->child[0]->parent = rs;
+			}
+			while (!v->key.empty())
+			{
+				rs->key.insert(0, v->key.remove(v->key.size() - 1));
+				if (rs->child[0])
+				{
+					rs->child[0]->parent = rs;
+				}
+			}
+			release(v);
+		}
+	}
+	solveUnderflow(p);
+	return;
 }
