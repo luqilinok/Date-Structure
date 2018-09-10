8bda1b304698945dfab83a151ebe96c537552241
luqilinok <luqilinok@live.cn>
2018-09-10 14:01:51 +0800

    1

--- 08.高级搜索树/RedBlack.h
+++ 08.高级搜索树/RedBlack.h
@@ -40,3 +40,54 @@ BinNodePosi(T) RedBlack<T>::insert(const T& e)
 	solveDoubleRed(x);
 	return x;
 }
+
+/*******************************************************************************************
+*  RedBlack双红调整算法：解决节点x与其父母均为红色的问题，分为两大类情况：
+	  RR-1:2次颜色翻转，2次黑高度更新，1~2次旋转，不再递归
+	  RR-2:3次颜色翻转，3次高度更新，0次旋转，需要递归
+******************************************************************************************/
+
+template<typename T>
+void RedBlack<T>::solveDoubleRed(BinNodePosi(T) x)
+{
+	if (IsRoot(*x))
+	{
+		_root->color = RB_BLACK;
+		_root->height++;
+		return;
+	}
+	BinNodePosi(T) p = x->parent;
+	if (IsBlack(p))
+	{
+		return;
+	}
+	BinNodePosi(T) g = p->parent;
+	BinNodePosi(T) u = uncle(x);
+	if (IsBlack(u))
+	{
+		if (IsLChild(*x) == IsLChild(*p))
+		{
+			p->color = RB_BLACK;
+		}
+		else
+		{
+			x->color = RB_BLACK;
+		}
+		g->color = RB_RED;
+
+		BinNodePosi(T) gg = g->parent;
+		Binnodeposi(T) f = FromParentTo(*g) = rotateAt(x);
+	}
+	else
+	{
+		p->color = RB_BLACK;
+		p->height++;
+		u->color = RB_BLACK;
+		u->height++;
+		if (!IsRoot(*g))
+		{
+			g->color = RB_RED;
+		}
+		solveDoubleRed(g);
+	}
+}
