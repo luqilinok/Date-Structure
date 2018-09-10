5b1c2af1842b8ea27cf84eeae56c984fba67359d
luqilinok <luqilinok@live.cn>
2018-09-10 11:32:38 +0800

    update

--- 08.高级搜索树/RedBlack.h
+++ 08.高级搜索树/RedBlack.h
@@ -5,18 +5,38 @@
     ((x).height==(ISRed(&x)?stature((x).lChild):stature((x).lChild)+1))\
 )
 
-
 #include"BST.h"
 template<typename T>
-class RedBlack:public BST<T>
+class RedBlack :public BST<T>
 {
-    protected:
-        void solveDoubleRed(BinNodePosi(T) x);
-        void solveDoubleBlack(BinNodePosi(T) x);
-        int updateHeight(BinNodePosi(T) x ;
+protected:
+	void solveDoubleRed(BinNodePosi(T) x);  //双红修正
+	void solveDoubleBlack(BinNodePosi(T) x);  //双黑修正
+	int updateHeight(BinNodePosi(T) x;  //更新节点x的高度
 
-    public:
-        BinNodePosi(T) insert(const T& e);
-        bool remove(const T& e);
+public:
+	BinNodePosi(T) insert(const T& e);  //插入
+	bool remove(const T& e);  //删除
+	//BST：search()等其余接口可以直接继续使用
 };
 
+template<typename T>
+int RedBlack<T>::updateHeight(BinNodePosi(T) x)
+{
+	x->height = max(stature(x->lChild), stature(x->rChild));
+	return IsBlack(x) ? x->height++ : x->height;
+}
+
+template<typename T>
+BinNodePosi(T) RedBlack<T>::insert(const T& e)
+{
+	BinNodePosi(T) &x = search(e);
+	if (x)
+	{
+		return x;
+	}
+	x = new BinNode<T>(e, _hot, NULL, NULL, -1);
+	_size++;
+	solveDoubleRed(x);
+	return x;
+}
