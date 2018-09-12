3c228057262033e406bb92970bb1e05d464f837b
luqilinok <luqilinok@live.cn>
2018-09-12 21:49:34 +0800

    up

--- 09.词典/Hashtable.h
+++ 09.词典/Hashtable.h
@@ -17,31 +17,30 @@ class Hashtable : public Dictionary<K, V>
     void rehash();
 
   public:
-    Hashtable(int c=5);
+    Hashtable(int c = 5);
     ~Hashtable();
-    int size() const{return N;}
-    bool put (K,V);
-    V* get(K k);
+    int size() const { return N; }
+    bool put(K, V);
+    V *get(K k);
     bool remove(K k);
 };
 
-
-template<typename K,typename V>
-Hashtable<K,V>::Hashtable(int c)
+template <typename K, typename V>
+Hashtable<K, V>::Hashtable(int c)
 {
-    M=primeNLT(c,1048576,"prime-1048576-bitmap.txt");
-    N=0;
-    ht=new Entry<K,V>*[M];
-    memeset(ht,0,sizeof(Entry<K,V>*)*M);
-    lazyRemovel=new Bitmap(M);
+    M = primeNLT(c, 1048576, "prime-1048576-bitmap.txt");
+    N = 0;
+    ht = new Entry<K, V> *[M];
+    memeset(ht, 0, sizeof(Entry<K, V> *) * M);
+    lazyRemovel = new Bitmap(M);
 }
 
-int primeNLT(int c,int n,char* file)
+int primeNLT(int c, int n, char *file)
 {
-    Bitmap B(file,n);
-    while(c<n)
+    Bitmap B(file, n);
+    while (c < n)
     {
-        if(B.test(c))
+        if (B.test(c))
         {
             c++;
         }
@@ -53,12 +52,12 @@ int primeNLT(int c,int n,char* file)
     }
 }
 
-template<typename K,typename V>
-Hashtable<K,V>::~Hashtable()
+template <typename K, typename V>
+Hashtable<K, V>::~Hashtable()
 {
-    for(int i=0;i<M;i++)
+    for (int i = 0; i < M; i++)
     {
-        if(ht[i])
+        if (ht[i])
         {
             release(ht[i]);
         }
@@ -67,34 +66,95 @@ Hashtable<K,V>::~Hashtable()
     }
 }
 
-template<typename K,typename V>
-V* Hashtable<K,V>::get(K k)
+template <typename K, typename V>
+V *Hashtable<K, V>::get(K k)
 {
-    int r=probe4Hit(k);
-    return ht[r]?&(ht[r]->value):NULL;
+    int r = probe4Hit(k);
+    return ht[r] ? &(ht[r]->value) : NULL;
 }
 
-template<typename K,typename V>
-int Hashtable<K,V>::probe4Hit(const K& k)
+template <typename K, typename V>
+int Hashtable<K, V>::probe4Hit(const K &k)
 {
-    int r=hashCode(k)%M;
-    while((ht[r]&&(k!=ht[r]->key))||(!ht(r)&&lazilyRemoved(r)))
-    r=(r+1)%M;
+    int r = hashCode(k) % M;
+    while ((ht[r] && (k != ht[r]->key)) || (!ht(r) && lazilyRemoved(r)))
+        r = (r + 1) % M;
     return r;
 }
 
-
-template<typename K,typename V>
-bool Hashtable<K,V>::remove(K k)
+template <typename K, typename V>
+bool Hashtable<K, V>::remove(K k)
 {
-    int r=probe4Hit(k);
-    if(!ht(r))
+    int r = probe4Hit(k);
+    if (!ht(r))
     {
         return false;
     }
     release(ht[r]);
-    ht[r]=NULL;
+    ht[r] = NULL;
     markAsRemoved(r);
     N--;
     return true;
+}
+
+template <typename K, typename V>
+bool Hashtable<K k, V v>
+{
+    if (ht[probe4Hit(k)])
+    {
+        return false;
+    }
+    int r = probe4Free(k);
+    ht[r] = new Entry<K, V>(k, v);
+    ++N;
+    if (N * 2 > M)
+    {
+        return true;
+    }
+}
+
+template <typename K, typename V>
+int Hashtable<K, V>::probe4Free(const K &k)
+{
+    int r = hashCode(k) % M;
+    while (ht[r])
+    {
+        r = (r + 1) % M;
+    }
+    return r;
+}
+
+template <typename K, typename V>
+void Hashtable<K, V>::rehash()
+{
+    int old_capacity = M;
+    Entry<K, V> **old_ht = ht;
+    M = primeNLT(2 * M, 1048576, "bitmap.txt");
+    N = 0;
+    ht = new Entry<K, V> *[M];
+    memeset(ht, 0, sizeof(Entry<K, V> *) * M);
+    release(lazilyRemoval);
+    lazyRemoval = new Bitmap(M);
+    for (int i = 0; i < old_capacity; i++)
+    {
+        if (old_ht[i])
+        {
+            put(old_ht[i]->key, old_ht->value);
+        }
+    }
+    release(old_ht);
+}
+
+static size_t hashCode(char c) { return (size_t)c; }
+static size_t hashCode(int k) { return (size_t)k; }
+static size_t hashCode(long long i) { return (size_t)((i >> 32) + (int)i); }
+static size_t hashCode(char s[])
+{
+    int h = 0;
+    for (size_t n = strlen(s), i = 0; i < n; i++)
+    {
+        h = (h << s) | (h >> 27);
+        h += (int)s[i];
+    }
+    return (size_t)h;
 }
