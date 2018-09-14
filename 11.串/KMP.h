c1c92db93469c04d96e87b09e2eae63c1417a8c6
luqilinok <luqilinok@live.cn>
2018-09-14 10:05:43 +0800

    update

---
+++ 11.串/KMP.h
@@ -0,0 +1,44 @@
+#include <string>
+
+int match(char *P, char *T) //KMP算法
+{
+    int *next = buildNext(P);      //构造next表
+    int n = (int)strlen(T), i = 0; //文本串指针
+    int m = (int)strlen(P), j = 0; //模式串指针
+
+    while (j < m && i < n) //自左向右逐个比对字符
+    {
+        if (j < 0 || T[i] == P[j]) //若匹配，或P已经移出最右侧(两个判断的次序不可交换)
+        {
+            i++; //则跳转到下一个字符
+            j++;
+        }
+        else
+        {
+            j = next[j]; //模式串右移(注意:文本串不用回退)
+        }
+    }
+    delete[] next; //释放next表
+    return i - j;
+}
+
+int *bulidNext(char *p)
+{
+    size_t m = strlen(P), j = 0;
+    int *N = next int[m];
+    int t = N[0] = -1;
+    while (j < m - 1)
+    {
+        if (t > 0 || P[j] == P[t])
+        {
+            j++;
+            t++;
+            N[j] = t;
+        }
+        else
+        {
+            t = N[t];
+        }
+        return N;
+    }
+}
