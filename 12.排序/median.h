2a2f353eca1a4eda2867b82334ff51073214017a
luqilinok <luqilinok@live.cn>
2018-09-18 14:32:30 +0800

    update

--- 12.排序/median.h
+++ 12.排序/median.h
@@ -1,22 +1,31 @@
 template <typename T>
-T median(Vector<T> &S1, int lo1, Vector<T> &S2, int lo2, int n)
+T median(Vector<T> &S1, int lo1, int n1, Vector<T> &S2, int lo2, int n2)
 {
-    if (n < 3)
+    if (n1 > n2)
     {
-        return trivialMedian(S1, lo1, n, S2, lo2, n);
+        return median(S2, lo2, n2, S1, lo1, n1);
     }
-    int mi1 = lo1 + n / 2;
-    int mi2 = lo2 + (n - 1) / 2;
-    if (S1[mi1] < S2[mi2])
+    if (n2 < 6)
     {
-        return median(S1, mi1, S2, lo2, n + lo1 - mi1);
+        return trivialMedian(S1, lo1, n1, S2, lo2, n2);
     }
-    else if (S1[mi1] > S2[mi2])
+    if (2 * n1 < n2)
     {
-        return median(S1, lo1, S2, mi2, n + lo2 - mi2);
+        return median(S1, lo1, n1, S2, lo2 + (n2 - n1 - 1) / 2, n1 + 2 - (n2 - n1) % 2);
+    }
+    int mi1 = lo1 + n1 / 2;
+    int mi2a = lo2 + (n1 - 1) / 2;
+    int mi2b = lo2 + n2 - 1 - n1 / 2;
+    if (S1[mi1] > S2[mi2b])
+    {
+        return median(S1, lo1, n1 / 2 + 1, S2, mi2a, n2 - (n1 - 1) / 2);
+    }
+    else if (S1[mi1] < S2[mi2a])
+    {
+        return median(S1, mi1, (ni + 1) / 2, S1, lo2, n2 - n1 / 2);
     }
     else
     {
-        return S1[mi1];
+        return median(S1, lo1, n1, S2, mi2a, n2 - (n1 - 1) / 2 * 2);
     }
 }
